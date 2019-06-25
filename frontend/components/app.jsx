010c3cb2cb8bd20e963e85d98176d3729939b217
Michael Warner <mwarner1040@gmail.com>
2019-06-25 13:46:02 -0400

    Adds the majority of user authentication. Still needs proper redirection upon successful login, but preserves the logged in user upon a refresh

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import GreetingContainer from './greeting/greeting_container';
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
+
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div>
-      <h1>Bundlr h1 tag dawg</h1>
+      <header>
+        <Link to="/" className="header-link">
+          <h1>Bundlr App</h1>
+        </Link>
+        <GreetingContainer />
+      </header>
+        <Switch>
+          <AuthRoute exact path="/login" component={LogInFormContainer} />
+          <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+        </Switch>
+
     </div>
   );
 };

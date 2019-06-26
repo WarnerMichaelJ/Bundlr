10c78df5062ea399b413fbd45d108b7328306093
Michael Warner <mwarner1040@gmail.com>
2019-06-26 12:22:39 -0400

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+User.create(name: 'Luka_Doncic', email: "Luka@gmail.com", password: '123456')
+User.create(name: 'Lebron_James', email: "Lebron@gmail.com", password: '123456')
+User.create(name: 'Steve_Nash', email: "Nash@gmail.com", password: '123456')
+User.create(name: 'Zion_Williamson', email: "zion@gmail.com", password: '123456')
+User.create(name: 'Stephen_Curry', email: "ChefCurry@gmail.com", password: '123456')
+User.create(name: 'Michael_Jordan', email: "Jordanesque@gmail.com", password: '123456')
+User.create(name: 'Jaren_Jackson_Jr', email: "JJJ@gmail.com", password: '123456')
+User.create(name: 'Allen_Iverson', email: "Iverson@gmail.com", password: '123456')

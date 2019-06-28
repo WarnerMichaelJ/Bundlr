a634065851467ff9ef37c84eced64e3c64905baa
Michael Warner <mwarner1040@gmail.com>
2019-06-28 13:21:34 -0400

    Adds different styling, as well as a demo log in button. Still an issue of the logo not showing up due to my not having a higher z-index on it than the background div. Will need to style the logo so it still shows up as clickable on the page

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -10,11 +10,22 @@ class SessionForm extends React.Component {
       email: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDemoLoginSubmit = this.handleDemoLoginSubmit.bind(this);
   }
 
   update(field) {
     return e => this.setState({
       [field]: e.currentTarget.value
+    });
+  }
+
+  handleDemoLoginSubmit(event) {
+    event.preventDefault();
+    
+    this.props.processForm({
+      username: "Luka_Doncic",
+      email: "Luka@gmail.com",
+      password: "123456"
     });
   }
 
@@ -49,40 +60,49 @@ class SessionForm extends React.Component {
         <br />
     </label> ) : null;
 
-    
+    const demoLogIn = this.props.formType === 'Log in' ? (
+
+      <button className="Demo-Login-Button" onClick={this.handleDemoLoginSubmit}>
+        Demo Log In
+      </button> 
+
+    ) : null;
 
     return (
-      <div className="login-form-container">
+      <div>
         <h3 className="Log-In-Or-Sign-Up"> {this.props.navLink} </h3>
-        <div className="background-of-session-form">
-          <form onSubmit={this.handleSubmit} className="login-form-box">
-            <h1 className="Bundlr-Big-Text"> bundlr </h1>
-            <br />
-            {this.renderErrors()}
-            <div className="login-form">
-              <br />
-              {email}
-              <label>
-                <input type="text"
-                  value={this.state.username}
-                  onChange={this.update('username')}
-                  className="login-input"
-                  placeholder="Username"
-                />
-              </label>
-              <br />
-              <label>
-                <input type="password"
-                  value={this.state.password}
-                  onChange={this.update('password')}
-                  className="login-input"
-                  placeholder="Password"
-                />
-              </label>
-              <br />
-              <input className="session-submit" type="submit" value={this.props.formType} />
-            </div>
-          </form>
+        <div className={"sign-up-login-background"}>
+          <div className="login-form-container">
+              <form onSubmit={this.handleSubmit} className="login-form-box">
+                <h1 className="Bundlr-Big-Text"> bundlr </h1>
+                <br />
+                {this.renderErrors()}
+                <div className="login-form">
+                  <br />
+                  {email}
+                  <label>
+                    <input type="text"
+                      value={this.state.username}
+                      onChange={this.update('username')}
+                      className="login-input"
+                      placeholder="Username"
+                    />
+                  </label>
+                  <br />
+                  <label>
+                    <input type="password"
+                      value={this.state.password}
+                      onChange={this.update('password')}
+                      className="login-input"
+                      placeholder="Password"
+                    />
+                  </label>
+                  <br />
+                  <input className="session-submit" type="submit" value={this.props.formType} />
+                  {demoLogIn}
+                </div>
+              </form>
+          </div>
         </div>
       </div>
     );
